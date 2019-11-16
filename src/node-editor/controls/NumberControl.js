359ab3ead82ed1d50423277c3ff13b7ad43aa934
Jason Reinhardt <jason.w.reinhardt@gmail.com>
2019-11-15 18:57:37 -0800

    Revert "Revert "191114-4""
    This reverts commit 4bdb04d093844cdc1670da041533b2e235104104.

--- src/node-editor/controls/NumberControl.js
+++ src/node-editor/controls/NumberControl.js
@@ -1,28 +1,5 @@
 import Rete from "rete";
-
-var VueNumberControl = {
-  props: ["readonly", "emitter", "ikey", "getData", "putData"],
-  template:
-    '<input type="number" :readonly="readonly" :value="value" @input="change($event)" @dblclick.stop="" @pointerdown.stop="" @pointermove.stop=""/>',
-  data() {
-    return {
-      value: 0
-    };
-  },
-  methods: {
-    change(e) {
-      this.value = +e.target.value;
-      this.update();
-    },
-    update() {
-      if (this.ikey) this.putData(this.ikey, this.value);
-      this.emitter.trigger("process");
-    }
-  },
-  mounted() {
-    this.value = this.getData(this.ikey);
-  }
-};
+import VueNumberControl from "./NumberControl.vue";
 
 export class NumberControl extends Rete.Control {
   constructor(emitter, key, readonly) {
